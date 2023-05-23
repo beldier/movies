de35310e0cae2382b8cccb351443a84552e2d77d
beldier <alvaro.yapu.cossio@gmail.com>
2023-05-23 10:06:38 -0400

    Migrate to StateFlow from LiveData

--- app/src/main/java/com/example/movies/model/MoviesRepository.kt
+++ app/src/main/java/com/example/movies/model/MoviesRepository.kt
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MoviesRepository(activity: AppCompatActivity) {
     private val apiKey = com.example.movies.BuildConfig.apiKey
-    private val regionRepository = RegionRepository(activity)
+//    private val regionRepository = RegionRepository(activity)
 
     suspend fun findPopularMovies() =
         RemoteConnection.service
             .listPopularMovies(
                 apiKey,
-                regionRepository.findLastRegion()
+                "US"
+//                regionRepository.findLastRegion()
             )
 }
--- app/src/main/java/com/example/movies/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/movies/ui/detail/DetailActivity.kt
@@ -4,10 +4,14 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.movies.databinding.ActivityDetailBinding
 import com.example.movies.model.Movie
 import com.example.movies.ui.common.loadUrl
-
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 class DetailActivity : AppCompatActivity() {
     companion object {
         const val MOVIE = "DetailActivity:movie"
@@ -25,7 +29,11 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.state.observe(this) { updateUI(it.movie) }
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state.collect { updateUI(it.movie) }
+            }
+        }
     }
 
     private fun updateUI(movie: Movie) = with(binding) {
