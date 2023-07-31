091387740f0ef35e04496a3e766497464f4cfcb4
beldier <alvaro.yapu.cossio@gmail.com>
2023-07-30 20:47:21 -0400

    Add annotation qualifier for api key

---
+++ app/src/main/java/com/example/movies/di/ApiKey.kt
@@ -0,0 +1,7 @@
+package com.example.movies.di
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.BINARY)
+@Qualifier
+annotation class ApiKey
--- app/src/main/java/com/example/movies/di/AppModule.kt
+++ app/src/main/java/com/example/movies/di/AppModule.kt
@@ -21,7 +21,7 @@ import javax.inject.Singleton
 object AppModule {
     @Provides
     @Singleton
-    @Named("apiKey")
+    @ApiKey
     fun provideApiKey(app: Application): String = app.getString(R.string.api_key)
 
     @Provides
@@ -37,7 +37,7 @@ object AppModule {
         MovieRoomDataSource(db.movieDao())
 
     @Provides
-    fun provideRemoteDataSource(@Named("apiKey") apiKey: String): MovieRemoteDataSource =
+    fun provideRemoteDataSource(@ApiKey apiKey: String): MovieRemoteDataSource =
         MovieServerDataSource(apiKey)
 
     @Provides
